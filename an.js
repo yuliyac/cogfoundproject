6fc741c2cd431f45f12da17aad67f90a9b6198eb
yuliya chernykhovskaya <yuliyac29@gmail.com>
2015-03-04 16:15:01 -0800

    new stuff

---
+++ an.js
@@ -0,0 +1,42 @@
+an.js$(function() {
+    // get the html elements we need to use
+    var container = $(".container");
+    var stimulus = $("#stimulus");
+    // set the delay parameters
+    var presentationInterval = 250;
+    var interStimulusInterval = 50;
+    
+    // initialize your stimuli here
+    var stimuli = [ 'A', 'B', 'C', 'D', 'E' ];
+    var i = 0;
+    
+    // update is the main stimulus presentation loop, it recursively calls
+    // itself as long as there are stimuli remaining    
+    function update() {
+        // wait until the isi has elapsed
+        container.delay(interStimulusInterval);
+        // then set the stimulus and increment the counter
+        // queue and dequeue makes this function "wait in line"
+        container.queue(function() {
+            stimulus.html(stimuli[i]);
+            i++;        // instead of adding 1, you could set i randomly or follow some other pattern
+            container.dequeue();
+        });
+        // show the stimulus, the argument is required to make show "wait in line", but it could
+        // be any small value, a large value will cause a visible animation
+        container.show(10);
+        // wait until the presentation interval has elapsed
+        container.delay(presentationInterval);
+        // hide the stimulus
+        container.hide(10);
+        // finally, check if there are remaining stimuli and recursively call this function
+        container.queue(function() {
+            if (i < stimuli.length)    // if you present "out of order" you'll have to modify this
+                update();              // one improvement would be to avoid recursion and use a callback
+            container.dequeue();
+        });
+    }
+    
+    // start presenting stimuli
+    update();
+});
